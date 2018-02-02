4be1a9508ecf4ae957324a00fa06321cd27898fc
svrrealism <svrrealists@gmail.com>
2018-02-01 19:52:59 -0500

    Create conditions necessary for database schema from model associations.

--- db/migrate/20180201005203_create_photos.rb
+++ db/migrate/20180201005203_create_photos.rb
@@ -1,7 +1,9 @@
 class CreatePhotos < ActiveRecord::Migration[5.1]
   def change
     create_table :photos do |t|
-
+      t.string :caption
+      t.string :image
+      t.references :album
       t.timestamps
     end
   end
