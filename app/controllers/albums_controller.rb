0e78ba9866b1cbb786d58776073dbe569b2b2858
svrrealism <svrrealists@gmail.com>
2018-02-02 23:09:06 -0500

    Add feature to signup users.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,12 @@
 class AlbumsController < ApplicationController
+
+  def index
+    @albums = Album.all
+  end
+
+  def show
+    @album = Album.find(params[:id])
+    @photos = @album.photos
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'albums#index'
+  get 'albums' => 'albums#index'
+  get 'albums/new' => 'albums#new'
+  get 'albums/:id' => 'albums#show', as: :album
+  post 'albums' => 'albums#create'
+
+  get 'signup' => 'users#new'
+  resources :users
 end
